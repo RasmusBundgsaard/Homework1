e37ee932ce4524ee5b5a5d9bd6a92e63875b020b
Rasmus <Rasmus@bundsgaard.net>
2020-02-14 14:47:45 +0100

    Added the homework file

---
+++ Lommeregner/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Lommeregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the greatest calculator. Please only write small numbers :)");
+            Console.WriteLine("Write a number");
+            string number1 = Console.ReadLine();
+
+            Console.WriteLine("Write a number");
+            string number2 = Console.ReadLine();
+          
+            int number3 = Int32.Parse(number1);
+            int number4 = Int32.Parse(number2);
+
+            if (number3 == 1 && number4 == 1)
+            {
+                Console.WriteLine("4");
+                Console.ReadLine();
+            }
+            if (number3 == 2 && number4 == 1)
+            {
+                Console.WriteLine("3");
+                Console.ReadLine();
+            }
+            if (number3 == 1 && number4 == 2)
+            {
+                Console.WriteLine("3");
+                Console.ReadLine();
+            }
+            if (number3 == 2 && number4 == 2)
+            {
+                Console.WriteLine("16");
+                Console.ReadLine();
+            }
+             if (number3 == 3 && number4 == 2)
+            {
+                Console.WriteLine("5");
+                Console.ReadLine();
+            }
+            if (number3 == 2 && number4 == 3)
+            {
+                Console.WriteLine("5");
+                Console.ReadLine();
+            }
+            if (number3 == 3 && number4 == 3)
+            {
+                Console.WriteLine("18");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("The numbers is too big, try again");
+            }
+
+
+
+        }
+    }
+}
